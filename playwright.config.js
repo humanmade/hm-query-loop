cbcb13ed76d8c10246c4341fa442f68fa486b991
Robert O'Rourke <robert@o-rourke.org>
2025-11-18 17:05:04 +0000

    Support JSON output for GH action CI

--- playwright.config.js
+++ playwright.config.js
@@ -20,7 +20,13 @@ module.exports = defineConfig({
 	/* Opt out of parallel tests on CI. */
 	workers: 1,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: process.env.CI ? [['list'], ['html']] : 'html',
+	reporter: process.env.CI
+		? [
+				['list'],
+				['html'],
+				['json', { outputFile: 'test-results/results.json' }],
+		  ]
+		: 'html',
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		baseURL,
